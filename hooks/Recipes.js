1655c9aadfa2c8a074a6514895f67475f612fc26
Camila Aparecida <114444962+CamilaApSoares@users.noreply.github.com>
2025-05-14 20:38:11 -0300

    Add files via upload

---
+++ hooks/Recipes.js
@@ -0,0 +1,39 @@
+
+
+export const Recipes = [
+    {
+        name: "Bolo de Mandioca",
+        descricao: "Bolo de Mandioca rápido e saboroso.",
+        recipe: "Coloque a mandioca no processador e bata, após bater, despeje a mandioca em um pano de prato e esprema para sair o excesso de líquido. No liquidificador Mondial coloque os ovos, o óleo, o leite de coco, o açúcar e a água. Bata bem e em uma tigela misture o conteúdo do liquidificador com a mandioca e o coco ralado. Despeje em uma assadeira untada e enfarinhada. Leve para a Air Fryer Forno a 180ºC por 35 minutos. Após isso seu bolo estará pronto para consumo."
+    },
+    {
+        name: "Petit Gateau",
+        descricao: "Esse doce delicioso conquista paladares em todo o mundo",
+        recipe: "Derreta a manteiga e o chocolate em banho-maria, bata os ovos e as gemas com açúcar na batedeira, até ficar bem claro. Junte o chocolate derretido e a farinha de trigo, misturando com uma espátula. Depois, unte as forminhas de empadinha, passe farinha de trigo e coloque a massa, preaqueça o forno e leve para assar de 6 a 10 minutos (em fogo alto) até os bolinhos crescerem, mas o meio deve ficar molinho. Deve-se desenformar ainda quente. Sirva diretamente no prato, acompanhado com sorvete de creme."
+    },
+    {
+        name: "Cuzcuz Nordestino no Micro-Ondas",
+        descricao: "Uma refeição leve e rápida, perfeita para a manhã.",
+        recipe: "Misture o flocão com a água e o sal. Deixe hidratar por 10 minutos.Coloque em uma cuscuzera ou tigela própria para micro-ondas. Cozinhe por 3 a 4 minutos no micro-ondas. Sirva com manteiga derretida."
+    },
+    {
+        name: "Crepioca",
+        descricao: "Opção rápida e forte para um almoço saúdavel.",
+        recipe: "Misture o ovo, a tapioca e o sal. Despeje na frigideira e cozinhe como uma panqueca. Adicione o recheio e dobre ao meio."
+    },
+    {
+        name: "Escondidinho de Carne Moída",
+        descricao: "Receita prática, perfeita para o almoço ou jantar.",
+        recipe: "Refogue a cebola e o alho no óleo. Adicione a carne moída e cozinhe bem. Tempere. Em um refratário, coloque a carne no fundo e cubra com o purê de batata. Finalize com queijo ralado e leve ao forno para gratinar (10–15 minutos)."
+    },
+    {
+        name: "Sanduíche Natural de Frango",
+        descricao: "Leve, refrescante e ótimo para lanches rápidos.",
+        recipe: "Misture o frango já cozido e desfiado com maionese, cenoura e temperos de sua preferencia. Recheie o pão e, se quiser, acrescente alface ou tomate. Sirva gelado ou leve à geladeira por 30 minutos."
+    },
+    {
+        name: "Banana Assada com Canela",
+        descricao: "Uma sobremesa fácil, saudável e muito saborosa.",
+        recipe: "Corte as bananas ao meio (no comprimento) e coloque numa assadeira. Polvilhe canela e mel por cima. Leve ao forno a 180°C por 10 a 15 minutos. Sirva com iogurte ou sorvete, se quiser."
+    }
+]
